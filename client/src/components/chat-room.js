0c92ba72918265bad29455509753fc6b75e981da
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-04-10 18:04:37 -0300

    add chat-room component

--- client/src/components/chat-room.js
+++ client/src/components/chat-room.js
@@ -1,36 +1,89 @@
 class ChatRoom extends HTMLElement {
     constructor() {
         super();
-        this.root = this.attachShadow({ mode: 'open' });
-    }
-    set chat(data) {
-        this.root.innerHTML = `
-              <style>
-               h2 {
-                font-family: Georgia, 'Times New Roman', Times, serif;
-                color: blue;
-              }
-              .chat-panel{
-                  height: 400px;
-                  border: 1px solid #ccc;
-              }
-              
-              </style>
+        this.message;
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <style>
+        h2 {
+         font-family: Georgia, 'Times New Roman', Times, serif;
+         color: blue;
+       }
+       .room {
+           display: flex;
+           height: 400px;
+           margin: 0 auto;
+           width:60%;
+           border: 1px solid #ccc;
+           
+       }
+       #users-online{
+            border-right: 1px solid #ccc; 
+            padding: 6px 3px;
+            width: 20%;
+       }
+       #message, #send{
+           margin: 8px auto;
+           padding: 8px;
+           display: flex;
+       }
+       #message{
+        width: 59%;
+       }
+       #send{
+        width: 60%;
+        border: none;
+        border-radius: .5em;
+        background: green;
+        color: #fff;
+        font-size: 1em;
+        text-align: center;
+        line-height: initial;
+        justify-content:center;
+        align-self:center;
+       }
+       </style>
 
-              <div>
-              <h2>Welcome!! ${data.user}</h2>
-              <h3>Chat Room</h3>
-              <div id='chat' class='chat-panel'></div>
-              </div>
-              <div id='users-online'></div>`;
+       <div>
+       <h2 id="welcome"></h2>
+       <h3>Chat Room</h3>
+       <section class='room'>
+       <div id='users-online'></div>
+       <div id='chat' class='chat-panel'></div>
+       </section> 
+       <input type='text' id='message' placeholder='Say something...' />
+       <button id='send'>Send</button>
+       </div>`;
+        this.shadowRoot.querySelector('#send').addEventListener('click', this._sendMessage);
     }
 
-    set online(data) {
-        const users = this.root.querySelector('#users-online');
-        const temp = data.map(u => (
-            `<p id='${u.id}'>${u.userName}</p>`
-        )).join('');
-        users.innerHTML = temp;
+    set title(user) {
+        console.log(user)
+        this.shadowRoot.querySelector('#welcome').textContent = `Welcome!! ${user.user}`;
+    }
+
+    set users(users) {
+        const template = `
+        ${users.map(u => (
+            `<p>${u.userName}</p>`
+        )).join('')}
+        `;
+
+        this.shadowRoot.querySelector('#users-online').innerHTML = template;
+
+    }
+
+    _sendMessage = () => {
+        const sendEvent = new Event('send');
+        this.message = this.shadowRoot.querySelector('#message').value;
+        if (this.message) {
+            this.dispatchEvent(sendEvent);
+        }
+    }
+    clearMessage() {
+        this.message = '';
+        this.shadowRoot.querySelector('#message').value = '';
+
     }
 }
 
--- client/src/js/socketManagerClient.js
+++ client/src/js/socketManagerClient.js
@@ -6,18 +6,23 @@ const error = document.querySelector('#error');
 const userName = document.querySelector('#userName');
 const submitUser = document.querySelector('#submitUser');
 const main = document.querySelector('#main');
-
+let chatRoom;
 submitUser.addEventListener('click', e => {
     const user = userName.value;
     if (user) {
         socket.emit('new user', user, callback => {
             if (callback) {
                 console.log('Succes');
-                const el = document.createElement('chat-room');
-                el.chat = { user };
-                main.innerHTML = null;
-                main.appendChild(el);
-
+                //const el = document.createElement('chat-room');
+                //el.title = { user };
+                main.innerHTML = '<chat-room></chat-room>';
+                //main.appendChild(el);
+                chatRoom = document.querySelector('chat-room');
+                chatRoom.title = { user };
+                chatRoom.addEventListener('send', e => {
+                    console.log('send..', chatRoom.message);
+                    chatRoom.clearMessage();
+                })
             }
         });
     } else {
@@ -29,6 +34,9 @@ submitUser.addEventListener('click', e => {
 socket.on('get users', data => {
     console.log('data :: ', data)
     const el = document.querySelector('chat-room');
-    el.online = data
-    main.appendChild(el);
-})
+    el.users = data;
+    //el.online = data
+    //main.appendChild(el);
+});
+
+
